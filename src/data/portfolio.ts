6772f856cf0d481c1e4a6ae9327c2d665f7aefea
Stiven Fortes <stiven.fortes@devgo.org>
2022-12-12 12:14:42 -0100

    refactor assets path

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -1,19 +1,19 @@
 export default [
   {
     tittle: 'AI Face Mask detection',
-    imgUrl: 'public/assets/mask-detection.png',
+    imgUrl: '/static/mask-detection.png',
     stack: ['Python', 'Keras', 'CNN'],
     link: 'https://github.com/Stivifortes/maskApp'
   },
   {
     tittle: 'Real Time Chat',
-    imgUrl: 'public/assets/realtime-chat.png',
+    imgUrl: '/static/realtime-chat.png',
     stack: ['Java', 'Java Swing', 'Socket'],
     link: 'https://www.youtube.com/watch?v=rXi4tf7AT78&ab_channel=StivenFortes'
   },
   {
     tittle: 'Contact App',
-    imgUrl: 'public/assets/agenda-app.png',
+    imgUrl: '/static/agenda-app.png',
     stack: ['Java', 'MySQL', 'Java Swing'],
     link: 'https://www.youtube.com/watch?v=ac6Se9S5AVs&ab_channel=StivenFortes'
   },
